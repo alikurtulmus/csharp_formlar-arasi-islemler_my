85dc9f66e08196ed494e05175ae1bf1238164620
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2022-08-17 20:18:11 +0300

    Add project files.

---
+++ csharp_formlar-arasi-islemler_my/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_formlar_arasi_islemler_my
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 fr = new Form2(); //formlar da birer sınıf olduğu için Form2 sınıfına ait bir nesne oluşturduk.
+            fr.Show(); //ilgili nesneye "Show" metodu uyguladık.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 fr = new Form2(); //bir önceki nesne türetme işlemini buton içinde yaptığımız için tekrar nesne üretmemiz gerekti.
+            //nesneyi global alanda türetseydik yeniden nesne üretmemize gerek kalmayacaktı
+            fr.mesaj = textBox1.Text;
+            fr.Show();
+            this.Hide(); //form1'i gizledik
+        }
+
+        //FORMLAR ARASI VERİ TAŞIMA
+
+    }
+}
---
+++ csharp_formlar-arasi-islemler_my/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_formlar_arasi_islemler_my
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string mesaj; //nesneyi public yapmazsak form1'den bu nesneye ulaşamayız
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = mesaj; //form2'deki label1'i mesaj nesnesine eşitledik
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 fr1 = new Form1();
+            fr1.Show();
+        }
+    }
+}
